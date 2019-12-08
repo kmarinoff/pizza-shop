0e012cfd91778c5c9241cc6642539172d94f72f4
kmarinoff <kaloyan.marinov@mentormate.com>
2019-12-08 19:34:56 +0200

    no dev tools

--- src/reduxStore/store.ts
+++ src/reduxStore/store.ts
@@ -1,10 +1,9 @@
 import { applyMiddleware, compose, createStore } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import { rootReducer } from "src/reduxStore";
 
 export const store = createStore(
   rootReducer,
 
-  composeWithDevTools(compose(applyMiddleware(thunk.withExtraArgument({}))))
+  compose(applyMiddleware(thunk.withExtraArgument({})))
 );
