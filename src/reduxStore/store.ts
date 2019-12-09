f225b3d792b84835ba17103d92f51ba84a65311c
kmarinoff <kaloyan.marinov@mentormate.com>
2019-12-09 09:12:38 +0200

    dev prod stuf

--- src/reduxStore/store.ts
+++ src/reduxStore/store.ts
@@ -1,9 +1,19 @@
 import { applyMiddleware, compose, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import { rootReducer } from "src/reduxStore";
 
-export const store = createStore(
-  rootReducer,
+const getStore =
+  !process.env.NODE_ENV || process.env.NODE_ENV === "development"
+    ? createStore(
+        rootReducer,
+        composeWithDevTools(
+          compose(applyMiddleware(thunk.withExtraArgument({})))
+        )
+      )
+    : createStore(
+        rootReducer,
+        compose(applyMiddleware(thunk.withExtraArgument({})))
+      );
 
-  compose(applyMiddleware(thunk.withExtraArgument({})))
-);
+export const store = getStore;
