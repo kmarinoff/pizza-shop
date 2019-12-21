2fe25a92a9f60fe199bc496af3d66fd17b41a158
kmarinoff <kaloyan.marinov@mentormate.com>
2019-12-21 16:03:24 +0200

    PIZZA-1: finishing sign in and log in component

--- src/components/LogIn/LogIn.tsx
+++ src/components/LogIn/LogIn.tsx
@@ -1,68 +1,135 @@
+import { Formik } from "formik";
 import React, { FC } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { signInWithFacebook, signInWithGoogle } from "src/setup";
+import { auth } from "src/setup";
+import * as yup from "yup";
+
+const schema = yup.object({
+  loginEmail: yup
+    .string()
+    .required("Email is a required field")
+    .max(50, "Email address must be at most 50 characters"),
+  loginPassword: yup
+    .string()
+    .required("Password is a required field")
+    .max(255, "Password must be at most 255 characters")
+    .min(6, "Password must be at least 6 characters long")
+});
 
 const LogIn: FC = () => {
   return (
     <>
-      <Form style={{ width: "100%" }}>
-        <Form.Group>
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            id="login-email"
-            type="email"
-            placeholder="Enter email"
-          />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
+      <Formik
+        validationSchema={schema}
+        onSubmit={async (values, action) => {
+          try {
+            await auth.signInWithEmailAndPassword(
+              values.loginEmail,
+              values.loginPassword
+            );
+          } catch (error) {
+            console.log("error:", error);
+          }
 
-        <Form.Group>
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            id="login-password"
-            type="password"
-            placeholder="Password"
-          />
-        </Form.Group>
-        <div className="conteiner">
-          <div className="row no-gutters d-flex justify-content-between align-items-top">
-            <div className="col-4">
-              <Button
-                variant="primary"
-                type="submit"
-                className="p-2 my-2"
-                style={{ width: "100%" }}
-              >
-                Login with Email
-              </Button>
+          action.resetForm({
+            values: {
+              loginEmail: "",
+              loginPassword: ""
+            }
+          });
+        }}
+        initialValues={{
+          loginEmail: "",
+          loginPassword: ""
+        }}
+      >
+        {({ handleSubmit, handleChange, values, touched, isValid, errors }) => (
+          <Form style={{ width: "100%" }} onSubmit={handleSubmit}>
+            <Form.Group>
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                id="login-email"
+                name="loginEmail"
+                value={values.loginEmail}
+                onChange={handleChange}
+                type="email"
+                placeholder="Enter email"
+                isValid={touched.loginEmail && !errors.loginEmail}
+                isInvalid={!!errors.loginEmail}
+              />
+              {!!errors.loginEmail ? (
+                <Form.Control.Feedback type="invalid">
+                  {errors.loginEmail}
+                </Form.Control.Feedback>
+              ) : (
+                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+              )}
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                id="login-password"
+                name="loginPassword"
+                value={values.loginPassword}
+                onChange={handleChange}
+                type="password"
+                placeholder="Password"
+                isValid={touched.loginPassword && !errors.loginPassword}
+                isInvalid={!!errors.loginPassword}
+              />
+              {!!errors.loginPassword ? (
+                <Form.Control.Feedback type="invalid">
+                  {errors.loginPassword}
+                </Form.Control.Feedback>
+              ) : (
+                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+              )}
+            </Form.Group>
+            <div className="conteiner">
+              <div className="row no-gutters d-flex justify-content-between align-items-top">
+                <div className="col-4">
+                  <Button
+                    variant="primary"
+                    type="submit"
+                    className="p-2 my-2"
+                    style={{ width: "100%" }}
+                    disabled={!isValid}
+                  >
+                    Login with Email
+                  </Button>
+                </div>
+                <div className="col-2 text-center my-2">Or:</div>
+                <div className="col-4">
+                  <Button
+                    variant="primary"
+                    type="button"
+                    className="p-2 my-2"
+                    style={{ width: "100%" }}
+                    onClick={signInWithGoogle}
+                  >
+                    Login with Google
+                  </Button>
+                  <Button
+                    variant="primary"
+                    type="button"
+                    className="p-2 my-2"
+                    style={{ width: "100%" }}
+                    onClick={signInWithFacebook}
+                  >
+                    Login with Facebook
+                  </Button>
+                </div>
+              </div>
             </div>
-            <div className="col-2 text-center my-2">Or:</div>
-            <div className="col-4">
-              <Button
-                variant="primary"
-                type="button"
-                className="p-2 my-2"
-                style={{ width: "100%" }}
-                onClick={signInWithGoogle}
-              >
-                Login with Google
-              </Button>
-              <Button
-                variant="primary"
-                type="button"
-                className="p-2 my-2"
-                style={{ width: "100%" }}
-                onClick={signInWithFacebook}
-              >
-                Login with Facebook
-              </Button>
-            </div>
-          </div>
-        </div>
-      </Form>
+          </Form>
+        )}
+      </Formik>
     </>
   );
 };
--- src/components/SignUp/SignUp.tsx
+++ src/components/SignUp/SignUp.tsx
@@ -6,20 +6,20 @@ import { auth, createUserProfileDocument } from "src/setup";
 import * as yup from "yup";
 
 const schema = yup.object({
-  displayName: yup
+  signUpDisplayName: yup
     .string()
     .required("Display name is a required field")
     .max(50, "Display name must be at most 50 characters"),
-  email: yup
+  signUpEmail: yup
     .string()
     .required("Email is a required field")
     .max(50, "Email address must be at most 50 characters"),
-  password: yup
+  signUpPassword: yup
     .string()
     .required("Password is a required field")
     .max(255, "Password must be at most 255 characters")
     .min(6, "Password must be at least 6 characters long"),
-  confirmPassword: yup
+  signUpConfirmPassword: yup
     .string()
     .required("Confirm password is a required field")
     .max(255, "Password must be at most 255 characters")
@@ -31,39 +31,62 @@ const SignUp: FC = () => {
     <>
       <Formik
         validationSchema={schema}
-        onSubmit={console.log}
+        onSubmit={async (values, action) => {
+          if (values.signUpPassword !== values.signUpConfirmPassword) {
+            alert("passwords don't match");
+            return;
+          }
+
+          try {
+            const {
+              user
+            }: {
+              user: any;
+            } = await auth.createUserWithEmailAndPassword(
+              values.signUpEmail,
+              values.signUpPassword
+            );
+
+            await createUserProfileDocument(user, {
+              displayName: values.signUpDisplayName
+            });
+          } catch (error) {
+            console.log("error:", error);
+          }
+
+          action.resetForm({
+            values: {
+              signUpDisplayName: "",
+              signUpEmail: "",
+              signUpPassword: "",
+              signUpConfirmPassword: ""
+            }
+          });
+        }}
         initialValues={{
-          displayName: "",
-          email: "",
-          password: "",
-          confirmPassword: ""
+          signUpDisplayName: "",
+          signUpEmail: "",
+          signUpPassword: "",
+          signUpConfirmPassword: ""
         }}
       >
-        {({
-          handleSubmit,
-          handleChange,
-          handleBlur,
-          values,
-          touched,
-          isValid,
-          errors
-        }) => (
+        {({ handleSubmit, handleChange, values, touched, isValid, errors }) => (
           <Form style={{ width: "100%" }} onSubmit={handleSubmit}>
             <Form.Group>
               <Form.Label>Display name</Form.Label>
               <Form.Control
                 id="signup-name"
-                name="displayName"
-                value={values.displayName}
+                name="signUpDisplayName"
+                value={values.signUpDisplayName}
                 onChange={handleChange}
                 type="text"
                 placeholder="Enter your display name"
-                isValid={touched.displayName && !errors.displayName}
-                isInvalid={!!errors.displayName}
+                isValid={touched.signUpDisplayName && !errors.signUpDisplayName}
+                isInvalid={!!errors.signUpDisplayName}
               />
-              {!!errors.displayName ? (
+              {!!errors.signUpDisplayName ? (
                 <Form.Control.Feedback type="invalid">
-                  {errors.displayName}
+                  {errors.signUpDisplayName}
                 </Form.Control.Feedback>
               ) : (
                 <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
@@ -74,17 +97,17 @@ const SignUp: FC = () => {
               <Form.Label>Email address</Form.Label>
               <Form.Control
                 id="signup-email"
-                name="email"
-                value={values.email}
+                name="signUpEmail"
+                value={values.signUpEmail}
                 onChange={handleChange}
                 type="email"
                 placeholder="Enter your email address"
-                isValid={touched.email && !errors.email}
-                isInvalid={!!errors.email}
+                isValid={touched.signUpEmail && !errors.signUpEmail}
+                isInvalid={!!errors.signUpEmail}
               />
-              {!!errors.email ? (
+              {!!errors.signUpEmail ? (
                 <Form.Control.Feedback type="invalid">
-                  {errors.email}
+                  {errors.signUpEmail}
                 </Form.Control.Feedback>
               ) : (
                 <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
@@ -98,30 +121,30 @@ const SignUp: FC = () => {
               <Form.Label>Password</Form.Label>
               <Form.Control
                 id="signup-password"
-                name="password"
-                value={values.password}
+                name="signUpPassword"
+                value={values.signUpPassword}
                 onChange={handleChange}
                 type="password"
                 placeholder="Enter your password"
                 isValid={
-                  touched.password &&
-                  !errors.password &&
-                  values.password === values.confirmPassword
+                  touched.signUpPassword &&
+                  !errors.signUpPassword &&
+                  values.signUpPassword === values.signUpConfirmPassword
                 }
                 isInvalid={
-                  !!errors.password ||
-                  values.password !== values.confirmPassword
+                  !!errors.signUpPassword ||
+                  values.signUpPassword !== values.signUpConfirmPassword
                 }
               />
-              {!!errors.password ||
-              values.password !== values.confirmPassword ? (
-                values.password !== values.confirmPassword ? (
+              {!!errors.signUpPassword ||
+              values.signUpPassword !== values.signUpConfirmPassword ? (
+                values.signUpPassword !== values.signUpConfirmPassword ? (
                   <Form.Control.Feedback type="invalid">
                     Password must match!
                   </Form.Control.Feedback>
                 ) : (
                   <Form.Control.Feedback type="invalid">
-                    {errors.password}
+                    {errors.signUpPassword}
                   </Form.Control.Feedback>
                 )
               ) : (
@@ -134,29 +157,29 @@ const SignUp: FC = () => {
               <Form.Control
                 id="signup-confirm-password"
                 name="confirmPassword"
-                value={values.confirmPassword}
+                value={values.signUpConfirmPassword}
                 onChange={handleChange}
                 type="password"
                 placeholder="Confirm your password"
                 isValid={
-                  touched.confirmPassword &&
-                  !errors.confirmPassword &&
-                  values.password === values.confirmPassword
+                  touched.signUpConfirmPassword &&
+                  !errors.signUpConfirmPassword &&
+                  values.signUpPassword === values.signUpConfirmPassword
                 }
                 isInvalid={
-                  !!errors.confirmPassword ||
-                  values.password !== values.confirmPassword
+                  !!errors.signUpConfirmPassword ||
+                  values.signUpPassword !== values.signUpConfirmPassword
                 }
               />
-              {!!errors.confirmPassword ||
-              values.password !== values.confirmPassword ? (
-                values.password !== values.confirmPassword ? (
+              {!!errors.signUpConfirmPassword ||
+              values.signUpPassword !== values.signUpConfirmPassword ? (
+                values.signUpPassword !== values.signUpConfirmPassword ? (
                   <Form.Control.Feedback type="invalid">
                     Password must match!
                   </Form.Control.Feedback>
                 ) : (
                   <Form.Control.Feedback type="invalid">
-                    {errors.confirmPassword}
+                    {errors.signUpConfirmPassword}
                   </Form.Control.Feedback>
                 )
               ) : (
@@ -172,7 +195,8 @@ const SignUp: FC = () => {
                     className="p-2 my-2"
                     style={{ width: "100%" }}
                     disabled={
-                      !isValid || values.password !== values.confirmPassword
+                      !isValid ||
+                      values.signUpPassword !== values.signUpConfirmPassword
                     }
                   >
                     Sign up
