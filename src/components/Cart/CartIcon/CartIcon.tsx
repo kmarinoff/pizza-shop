a8ae902c1a1998df7785ca2fa35f81dba80d48f8
kmarinoff <kaloyan.marinov@mentormate.com>
2020-01-08 12:16:30 +0200

    PIZZA-6: add cart count for all the items

--- src/components/Cart/CartIcon/CartIcon.tsx
+++ src/components/Cart/CartIcon/CartIcon.tsx
@@ -1,10 +1,23 @@
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { FC } from "react";
+import { useSelector } from "react-redux";
 // import { ReactComponent as ShopingBagIcon } from "src/assets/shopping-bag.svg";
 import "./CartIcon.scss";
 
 const CartIcon: FC = () => {
+  const cart: any[] = useSelector((state: any) => state.cart);
+
+  const totalCartItems = cart.reduce(
+    (
+      accumulator: number,
+      currentItem: { id: number; name: string; price: number; count: number }
+    ) => {
+      return accumulator + currentItem.count;
+    },
+    0
+  );
+
   return (
     <div className="cart-icon">
       <FontAwesomeIcon
@@ -16,7 +29,7 @@ const CartIcon: FC = () => {
         }}
         icon={faShoppingCart}
       />
-      <span className="item-count">0</span>
+      <span className="item-count">{totalCartItems}</span>
     </div>
   );
 };
