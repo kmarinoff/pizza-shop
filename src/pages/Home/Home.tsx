ad705a814ebd94296384347de7ac7d23e7c35632
kmarinoff <kaloyan.marinov@mentormate.com>
2020-01-21 12:50:10 +0200

    PIZZA-6: add redux-persist to project in order to persist cart state

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,17 +2,17 @@ import React, { FC, useEffect } from "react";
 import Spinner from "react-bootstrap/Spinner";
 import { useDispatch, useSelector } from "react-redux";
 import { getPizzasRequest } from "src/reduxStore";
-import { createLoadingSelector } from "src/reduxStore/actions/selectors";
+// import { createLoadingSelector } from "src/reduxStore/actions/selectors";
 import { Pizza } from "src/types";
 import { PizzaList } from "./components/PizzaList";
 
 const Home: FC = () => {
   const dispatch = useDispatch();
-  const loadingSelectors = createLoadingSelector(["GET_PIZZAS"]);
+  // const loadingSelectors = createLoadingSelector(["GET_PIZZAS"]);
   const pizzas: Pizza[] = useSelector((state: any) => state.pizzas);
-  const isLoading = useSelector((state: any) => ({
-    isFetching: loadingSelectors(state)
-  }));
+  // const isLoading = useSelector((state: any) => ({
+  //   isFetching: loadingSelectors(state)
+  // }));
 
   // console.log("isLoading:", isLoading);
 
