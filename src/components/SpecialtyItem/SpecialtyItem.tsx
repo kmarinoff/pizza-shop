0d5cf1be6beced91e30b6972e1b2ded45cfbb1cd
kmarinoff <kaloyan.marinov@mentormate.com>
2020-04-19 01:32:16 +0300

    PIZZA-6: add specialties section

--- src/components/SpecialtyItem/SpecialtyItem.tsx
+++ src/components/SpecialtyItem/SpecialtyItem.tsx
@@ -14,11 +14,37 @@ const SpecialtyItem: React.FC<SpecialtyItemProps> = ({
   specialtyBody
 }) => {
   return (
-    <div style={{ width: "300px" }}>
-      <img width="300" height="200" src={imageSrc} alt={specialtyType} />
-      <div className="specialty-title">{specialtyTitle}</div>
-      <hr />
-      <p>{specialtyBody}</p>
+    <div
+      style={{
+        width: "300px",
+        backgroundColor: "white",
+        boxShadow: "0px 0px 10px 0px rgba(0,0,0,0.2)",
+        borderRadius: "10px"
+      }}
+    >
+      <img
+        width="300"
+        height="200"
+        src={imageSrc}
+        alt={specialtyType}
+        style={{ borderTopLeftRadius: "10px", borderTopRightRadius: "10px" }}
+      />
+      <div className="content" style={{ padding: "10px 20px" }}>
+        <div
+          className="specialty-title"
+          style={{
+            fontFamily: "Droid Serif",
+            fontSize: "1.2em",
+            letterSpacing: "1px"
+          }}
+        >
+          {specialtyTitle}
+        </div>
+        <hr style={{ color: "#A0CE54" }} />
+        <p style={{ fontFamily: "Fira Sans", color: "#959595" }}>
+          {specialtyBody}
+        </p>
+      </div>
     </div>
   );
 };
