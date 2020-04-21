60fcaee9756cca1373410c5a5a891fbe990e9c12
kmarinoff <kaloyan.marinov@mentormate.com>
2020-04-21 14:24:08 +0300

    PIZZA-6: add react-transition-group and react-custom-scrollbar to order book

--- src/components/OrderBook/OrderBook.tsx
+++ src/components/OrderBook/OrderBook.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import ToggleButton from "react-bootstrap/ToggleButton";
 import ToggleButtonGroup from "react-bootstrap/ToggleButtonGroup";
+import { Scrollbars } from "react-custom-scrollbars";
 import { CSSTransition } from "react-transition-group";
 import { pizzaArray } from "./pizzaArray";
 import "./styles.scss";
 
 const OrderBook: React.FC = () => {
-  const [value, setValue] = React.useState(1);
+  const [value, setValue] = React.useState(0);
   const handleChange = (val: number) => setValue(val);
+
+  React.useEffect(() => {
+    setValue(1);
+  }, []);
 
   return (
     <>
@@ -22,7 +27,6 @@ const OrderBook: React.FC = () => {
         <ToggleButtonGroup
           type="radio"
           name="options"
-          defaultValue={value}
           value={value}
           onChange={handleChange}
         >
@@ -45,46 +49,46 @@ const OrderBook: React.FC = () => {
           margin: "50px 0"
         }}
       />
-      <div style={{ position: "relative", overflow: "hidden" }}>
+      <div style={{ overflow: "hidden", position: "relative" }}>
         <CSSTransition
           in={value === 1}
-          timeout={5000}
-          classNames="menu-primary"
+          timeout={300}
+          classNames="pizza-menu"
           unmountOnExit
         >
-          <div className="menu">
-            <div
-              style={{
-                backgroundColor: "pink",
-                display: "flex",
-                flexDirection: "row",
-                fontFamily: "Fira Sans",
-                width: "940px"
-              }}
-            >
-              <div>
-                <div
-                  style={{
-                    backgroundColor: "#2E2E2E",
-                    padding: "15px 30px",
-                    color: "#ffffff",
-                    display: "flex",
-                    justifyContent: "space-between",
-                    alignItems: "center"
-                  }}
-                >
-                  <div>Added products: 1</div>
-                  <button>Pre-Order Now</button>
-                </div>
-                <div
-                  className="left-book-side"
-                  style={{
-                    backgroundColor: "#2E2E2E",
-                    padding: "30px",
-                    overflow: "auto",
-                    height: "530px"
-                  }}
-                >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              fontFamily: "Fira Sans",
+              width: "940px"
+            }}
+          >
+            <div>
+              <div className="menu-header"> TEST </div>
+              <div
+                style={{
+                  backgroundColor: "#2E2E2E",
+                  padding: "15px 30px",
+                  color: "#ffffff",
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center"
+                }}
+              >
+                <div>Added products: 1</div>
+                <button>Pre-Order Now</button>
+              </div>
+              <div
+                className="left-book-side"
+                style={{
+                  backgroundColor: "#2E2E2E",
+                  padding: "30px",
+                  height: "530px",
+                  width: "475px"
+                }}
+              >
+                <Scrollbars>
                   {pizzaArray.map(pizza => (
                     <div
                       key={pizza.id}
@@ -134,47 +138,47 @@ const OrderBook: React.FC = () => {
                       </div>
                     </div>
                   ))}
-                </div>
+                </Scrollbars>
               </div>
-              <div
-                className="right-book-side"
-                style={{ backgroundColor: "#ffffff" }}
-              >
-                <img
-                  src={require("src/assets/order-book/pizza-bg.jpg")}
-                  alt="pizzaBackground"
-                  style={{ width: "100%" }}
-                />
-                <div style={{ margin: "20px" }}>
-                  <div
-                    style={{
-                      marginTop: "3px",
-                      marginBottom: "27px",
-                      fontSize: "20px",
-                      color: "#222222",
-                      lineHeight: "30px",
-                      fontWeight: 500,
-                      textAlign: "center"
-                    }}
-                  >
-                    Pizza
-                  </div>
-                  <div
-                    style={{
-                      fontSize: "16px",
-                      color: "#787878",
-                      fontWeight: 300,
-                      lineHeight: "24px"
-                    }}
-                  >
-                    The origin of the word pizza is uncertain and debated. One
-                    popular suggestion holds that it originates from the Greek
-                    pita (derived from ancient Greek pēktos, πηκτός, meaning
-                    "solid" or "clotted"). The ancient Greeks covered their
-                    bread with oils, herbs and cheese. The Romans developed
-                    placenta, a sheet of dough topped with cheese and honey and
-                    flavored with bay leaves.
-                  </div>
+            </div>
+            <div
+              className="right-book-side"
+              style={{ backgroundColor: "#ffffff" }}
+            >
+              <img
+                src={require("src/assets/order-book/pizza-bg.jpg")}
+                alt="pizzaBackground"
+                style={{ width: "100%" }}
+              />
+              <div style={{ margin: "20px" }}>
+                <div
+                  style={{
+                    marginTop: "3px",
+                    marginBottom: "27px",
+                    fontSize: "20px",
+                    color: "#222222",
+                    lineHeight: "30px",
+                    fontWeight: 500,
+                    textAlign: "center"
+                  }}
+                >
+                  Pizza
+                </div>
+                <div
+                  style={{
+                    fontSize: "16px",
+                    color: "#787878",
+                    fontWeight: 300,
+                    lineHeight: "24px"
+                  }}
+                >
+                  The origin of the word pizza is uncertain and debated. One
+                  popular suggestion holds that it originates from the Greek
+                  pita (derived from ancient Greek pēktos, πηκτός, meaning
+                  "solid" or "clotted"). The ancient Greeks covered their bread
+                  with oils, herbs and cheese. The Romans developed placenta, a
+                  sheet of dough topped with cheese and honey and flavored with
+                  bay leaves.
                 </div>
               </div>
             </div>
@@ -183,43 +187,43 @@ const OrderBook: React.FC = () => {
 
         <CSSTransition
           in={value === 2}
-          timeout={5000}
-          classNames="menu-primary"
+          timeout={300}
+          classNames="burger-menu"
           unmountOnExit
         >
-          <div className="menu">
-            <div
-              style={{
-                backgroundColor: "pink",
-                display: "flex",
-                flexDirection: "row",
-                fontFamily: "Fira Sans",
-                width: "940px"
-              }}
-            >
-              <div>
-                <div
-                  style={{
-                    backgroundColor: "#2E2E2E",
-                    padding: "15px 30px",
-                    color: "#ffffff",
-                    display: "flex",
-                    justifyContent: "space-between",
-                    alignItems: "center"
-                  }}
-                >
-                  <div>Added products: 1</div>
-                  <button>Pre-Order Now</button>
-                </div>
-                <div
-                  className="left-book-side"
-                  style={{
-                    backgroundColor: "#2E2E2E",
-                    padding: "30px",
-                    overflow: "auto",
-                    height: "530px"
-                  }}
-                >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              fontFamily: "Fira Sans",
+              width: "940px"
+            }}
+          >
+            <div>
+              <div className="menu-header"> TEST </div>
+              <div
+                style={{
+                  backgroundColor: "#2E2E2E",
+                  padding: "15px 30px",
+                  color: "#ffffff",
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center"
+                }}
+              >
+                <div>Added products: 1</div>
+                <button>Pre-Order Now</button>
+              </div>
+              <div
+                className="left-book-side"
+                style={{
+                  backgroundColor: "#2E2E2E",
+                  padding: "30px",
+                  height: "530px",
+                  width: "475px"
+                }}
+              >
+                <Scrollbars>
                   {pizzaArray.map(pizza => (
                     <div
                       key={pizza.id}
@@ -269,47 +273,47 @@ const OrderBook: React.FC = () => {
                       </div>
                     </div>
                   ))}
-                </div>
+                </Scrollbars>
               </div>
-              <div
-                className="right-book-side"
-                style={{ backgroundColor: "#ffffff" }}
-              >
-                <img
-                  src={require("src/assets/order-book/pizza-bg.jpg")}
-                  alt="pizzaBackground"
-                  style={{ width: "100%" }}
-                />
-                <div style={{ margin: "20px" }}>
-                  <div
-                    style={{
-                      marginTop: "3px",
-                      marginBottom: "27px",
-                      fontSize: "20px",
-                      color: "#222222",
-                      lineHeight: "30px",
-                      fontWeight: 500,
-                      textAlign: "center"
-                    }}
-                  >
-                    Burger
-                  </div>
-                  <div
-                    style={{
-                      fontSize: "16px",
-                      color: "#787878",
-                      fontWeight: 300,
-                      lineHeight: "24px"
-                    }}
-                  >
-                    The origin of the word pizza is uncertain and debated. One
-                    popular suggestion holds that it originates from the Greek
-                    pita (derived from ancient Greek pēktos, πηκτός, meaning
-                    "solid" or "clotted"). The ancient Greeks covered their
-                    bread with oils, herbs and cheese. The Romans developed
-                    placenta, a sheet of dough topped with cheese and honey and
-                    flavored with bay leaves.
-                  </div>
+            </div>
+            <div
+              className="right-book-side"
+              style={{ backgroundColor: "#ffffff" }}
+            >
+              <img
+                src={require("src/assets/order-book/pizza-bg.jpg")}
+                alt="pizzaBackground"
+                style={{ width: "100%" }}
+              />
+              <div style={{ margin: "20px" }}>
+                <div
+                  style={{
+                    marginTop: "3px",
+                    marginBottom: "27px",
+                    fontSize: "20px",
+                    color: "#222222",
+                    lineHeight: "30px",
+                    fontWeight: 500,
+                    textAlign: "center"
+                  }}
+                >
+                  Burger
+                </div>
+                <div
+                  style={{
+                    fontSize: "16px",
+                    color: "#787878",
+                    fontWeight: 300,
+                    lineHeight: "24px"
+                  }}
+                >
+                  The origin of the word pizza is uncertain and debated. One
+                  popular suggestion holds that it originates from the Greek
+                  pita (derived from ancient Greek pēktos, πηκτός, meaning
+                  "solid" or "clotted"). The ancient Greeks covered their bread
+                  with oils, herbs and cheese. The Romans developed placenta, a
+                  sheet of dough topped with cheese and honey and flavored with
+                  bay leaves.
                 </div>
               </div>
             </div>
@@ -318,43 +322,43 @@ const OrderBook: React.FC = () => {
 
         <CSSTransition
           in={value === 3}
-          timeout={5000}
-          classNames="menu-primary"
+          timeout={300}
+          classNames="kebap-menu"
           unmountOnExit
         >
-          <div className="menu">
-            <div
-              style={{
-                backgroundColor: "pink",
-                display: "flex",
-                flexDirection: "row",
-                fontFamily: "Fira Sans",
-                width: "940px"
-              }}
-            >
-              <div>
-                <div
-                  style={{
-                    backgroundColor: "#2E2E2E",
-                    padding: "15px 30px",
-                    color: "#ffffff",
-                    display: "flex",
-                    justifyContent: "space-between",
-                    alignItems: "center"
-                  }}
-                >
-                  <div>Added products: 1</div>
-                  <button>Pre-Order Now</button>
-                </div>
-                <div
-                  className="left-book-side"
-                  style={{
-                    backgroundColor: "#2E2E2E",
-                    padding: "30px",
-                    overflow: "auto",
-                    height: "530px"
-                  }}
-                >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              fontFamily: "Fira Sans",
+              width: "940px"
+            }}
+          >
+            <div>
+              <div className="menu-header"> TEST </div>
+              <div
+                style={{
+                  backgroundColor: "#2E2E2E",
+                  padding: "15px 30px",
+                  color: "#ffffff",
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center"
+                }}
+              >
+                <div>Added products: 1</div>
+                <button>Pre-Order Now</button>
+              </div>
+              <div
+                className="left-book-side"
+                style={{
+                  backgroundColor: "#2E2E2E",
+                  padding: "30px",
+                  height: "530px",
+                  width: "475px"
+                }}
+              >
+                <Scrollbars>
                   {pizzaArray.map(pizza => (
                     <div
                       key={pizza.id}
@@ -404,47 +408,47 @@ const OrderBook: React.FC = () => {
                       </div>
                     </div>
                   ))}
-                </div>
+                </Scrollbars>
               </div>
-              <div
-                className="right-book-side"
-                style={{ backgroundColor: "#ffffff" }}
-              >
-                <img
-                  src={require("src/assets/order-book/pizza-bg.jpg")}
-                  alt="pizzaBackground"
-                  style={{ width: "100%" }}
-                />
-                <div style={{ margin: "20px" }}>
-                  <div
-                    style={{
-                      marginTop: "3px",
-                      marginBottom: "27px",
-                      fontSize: "20px",
-                      color: "#222222",
-                      lineHeight: "30px",
-                      fontWeight: 500,
-                      textAlign: "center"
-                    }}
-                  >
-                    Kebap
-                  </div>
-                  <div
-                    style={{
-                      fontSize: "16px",
-                      color: "#787878",
-                      fontWeight: 300,
-                      lineHeight: "24px"
-                    }}
-                  >
-                    The origin of the word pizza is uncertain and debated. One
-                    popular suggestion holds that it originates from the Greek
-                    pita (derived from ancient Greek pēktos, πηκτός, meaning
-                    "solid" or "clotted"). The ancient Greeks covered their
-                    bread with oils, herbs and cheese. The Romans developed
-                    placenta, a sheet of dough topped with cheese and honey and
-                    flavored with bay leaves.
-                  </div>
+            </div>
+            <div
+              className="right-book-side"
+              style={{ backgroundColor: "#ffffff" }}
+            >
+              <img
+                src={require("src/assets/order-book/pizza-bg.jpg")}
+                alt="pizzaBackground"
+                style={{ width: "100%" }}
+              />
+              <div style={{ margin: "20px" }}>
+                <div
+                  style={{
+                    marginTop: "3px",
+                    marginBottom: "27px",
+                    fontSize: "20px",
+                    color: "#222222",
+                    lineHeight: "30px",
+                    fontWeight: 500,
+                    textAlign: "center"
+                  }}
+                >
+                  Kebap
+                </div>
+                <div
+                  style={{
+                    fontSize: "16px",
+                    color: "#787878",
+                    fontWeight: 300,
+                    lineHeight: "24px"
+                  }}
+                >
+                  The origin of the word pizza is uncertain and debated. One
+                  popular suggestion holds that it originates from the Greek
+                  pita (derived from ancient Greek pēktos, πηκτός, meaning
+                  "solid" or "clotted"). The ancient Greeks covered their bread
+                  with oils, herbs and cheese. The Romans developed placenta, a
+                  sheet of dough topped with cheese and honey and flavored with
+                  bay leaves.
                 </div>
               </div>
             </div>
