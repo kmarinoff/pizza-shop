f919fc380ae3b70d7204164eabf7d5c8ee3c06f4
kmarinoff <kaloyan.marinov@mentormate.com>
2020-06-11 19:09:27 +0300

    PIZZA-10: fix cart reducer

--- src/reduxStore/modules/cart/cartReducer.tsx
+++ src/reduxStore/modules/cart/cartReducer.tsx
@@ -6,10 +6,14 @@ import {
 import { CartItem, Pizza } from "src/types";
 
 export interface ICartReducer {
+  isFetching: boolean;
+  isFailed: boolean;
   cart: CartItem[];
 }
 
 const initState: ICartReducer = {
+  isFetching: false,
+  isFailed: false,
   cart: []
 };
 
@@ -17,16 +21,20 @@ const cartReducer = (state = initState, action: any) => {
   switch (action.type) {
     case ADD_TO_CART: {
       if (state.cart.find((item: Pizza) => item.id === action.payload.id)) {
-        return state.cart.map((item: any) =>
-          item.id === action.payload.id
-            ? {
-                ...item,
-                count: item.count + 1
-              }
-            : item
-        );
+        return {
+          ...state,
+          cart: state.cart.map((item: any) =>
+            item.id === action.payload.id
+              ? {
+                  ...item,
+                  count: item.count + 1
+                }
+              : item
+          )
+        };
       } else {
         return {
+          ...state,
           cart: [...state.cart, { ...action.payload, count: 1 }]
         };
       }
@@ -34,16 +42,19 @@ const cartReducer = (state = initState, action: any) => {
 
     case REMOVE_FROM_CART: {
       if (state.cart.find((item: Pizza) => item.id === action.payload.id)) {
-        return state.cart
-          .map((item: any) =>
-            item.id === action.payload.id
-              ? {
-                  ...item,
-                  count: item.count - 1
-                }
-              : item
-          )
-          .filter((item: any) => item.count > 0);
+        return {
+          ...state,
+          cart: state.cart
+            .map((item: any) =>
+              item.id === action.payload.id
+                ? {
+                    ...item,
+                    count: item.count - 1
+                  }
+                : item
+            )
+            .filter((item: any) => item.count > 0)
+        };
       }
       return state;
     }
